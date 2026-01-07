e4cdd869b57eb518cc69c2649630e2c3dcb2e358
Turab Ali <turabalidev004@gmail.com>
2026-01-07 22:16:57 +0500

    OOP(class and constructer)

--- DataStructure.py
+++ DataStructure.py
@@ -62,6 +62,6 @@ for key, value in person.items():
 
 # Dictionary comprehension
 # squares = {x: x**2 for x in range(5)}
-squares = {x*2 for x in range(5)}
-print(squares)
+# squares = {x*2 for x in range(5)}
+# print(squares)
 # {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
