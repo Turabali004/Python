85ff97113d6366f11303edd01a5a4a5d382d7468
Turab Ali <turabalidev004@gmail.com>
2026-03-17 14:20:01 +0500

    practice loop and problem solving through it

--- Loops.py
+++ Loops.py
@@ -1,8 +1,8 @@
-data = {
-      "name": "Turab",
-      "stack": "Mern Stack",
-      "experience": "2 years"
-}
+# data = {
+#       "name": "Turab",
+#       "stack": "Mern Stack",
+#       "experience": "2 years"
+# }
 
 
 # def listValue(**kwargs):
@@ -36,3 +36,109 @@ data = {
 #       # print(numbers(0))
 
 # listNum(*valueOfNum)
+
+
+
+
+# number = 6;
+# newNum = 1;
+
+# for num in range(1, number+1):
+#       newNum = newNum * num
+# print(newNum)
+
+
+# string = "javascript"
+# matchStr = 0
+
+# for str in string:
+#       reverseString = str + reverseString
+# print(reverseString)
+
+# for str in range(len(string;)):
+#       print(string[str])
+
+
+# for str in string:
+#       # print(string.count("t"))
+#       break;
+
+
+# string = "devzone"
+# # print(range(len(string)))
+# # matchStr = 0
+
+# for i in range(len(string)):
+#     print(range(i))
+#     for j in range(i):
+#         if string[i] == string[j]:
+#             # print("string[i]", string[i])
+#             # print("string[j]", string[j])
+#             print("First repeating letter:", string[i])
+#             break
+#     else:
+#         continue
+#     break
+# print(matchStr)
+#loop j in javascript
+# loop "j" "a" "v" "a" "s" "c" "r" "i" "p" "t" in javascript
+# if string[str] == newStr
+# matchStr = matchStr + 1
+
+# matchStr = 1
+
+
+#loop a in javascript
+# loop "j" "a" "v" "a" "s" "c" "r" "i" "p" "t" in javascript
+# if string[str] == newStr
+# matchStr = matchStr + 1
+
+# matchStr = 2
+
+#loop v in javascript
+# loop "j" "a" "v" "a" "s" "c" "r" "i" "p" "t" in javascript
+# if string[str] == newStr
+# matchStr = matchStr + 1
+
+# matchStr = 1
+
+
+
+#                                       while loop
+
+# name = "javascript"
+# number = 5
+# factorial = 1
+
+# while number > 0:
+#         factorial = factorial * number
+#         number = number - 1
+# print(factorial)
+    
+
+# 5 fac = 1*5 num = 4
+# 4 fac = 5*4 num = 3
+# 3 fac = 20*3 num = 2
+# 2 fac = 60*2 num = 1
+# 1 fac = 120*1 num = 1
+
+
+# while True:
+#     num = int(input("Enter a number into 1 to 10: "))
+#     if 1 <= num <= 10:
+#         print("Your num is: " ,num)
+#         break
+#     else: 
+#         print("Invalid number")
+
+number = 29
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is not a prime number")
+            break
+    else:
+        print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
